0f220eea95a6406d9317905756f08cdc68fe9c4f
Cherry <qz2338@columbia.edu>
2019-03-18 18:02:19 -0400

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/axjllt/catkin_ws/src
+# Build directory: /home/axjllt/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(rbx1/rbx1)
+subdirs(rbx1/rbx1_speech)
+subdirs(rbx1/rbx1_bringup)
+subdirs(rbx1/rbx1_dynamixels)
+subdirs(rbx1/rbx1_vision)
+subdirs(rbx1/rbx1_apps)
+subdirs(rbx1/rbx1_nav)
+subdirs(rbx1/rbx1_description)
